63b249a57b58be5fc1a9912af7138e8b9080f412
rustoma <rutoma00@gmail.com>
2023-09-02 13:18:20 +0200

    feat: add refresh_token to migration

--- internal/db/migrations/000001_init.up.sql
+++ internal/db/migrations/000001_init.up.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
+    "refresh_token" TEXT,
     "password_hash" TEXT NOT NULL,
     "role_id" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
