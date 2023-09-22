360a46cb6e55181e06cbe42b1253fe261241c914
rustoma <rutoma00@gmail.com>
2023-09-22 06:53:28 +0200

    feat: add article controller

---
+++ internal/controllers/article.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/rustoma/octo-pulse/internal/api"
+	"github.com/rustoma/octo-pulse/internal/services"
+	"github.com/rustoma/octo-pulse/internal/tasks"
+)
+
+type ArticleController struct {
+	articleService services.ArticleService
+	articleTasks   tasks.ArticleTasker
+}
+
+func NewArticleController(articleService services.ArticleService, articleTasks tasks.ArticleTasker) *ArticleController {
+	return &ArticleController{
+		articleService: articleService,
+		articleTasks:   articleTasks,
+	}
+}
+
+func (c *ArticleController) HandleGenerateDescritption(w http.ResponseWriter, r *http.Request) error {
+	pageIdParam := chi.URLParam(r, "id")
+	pageId, err := strconv.Atoi(pageIdParam)
+
+	if err != nil {
+		return api.Error{Err: "bad request", Status: http.StatusBadRequest}
+	}
+
+	err = c.articleTasks.NewGenerateDescriptionTask(pageId)
+
+	if err != nil {
+		return api.Error{Err: err.Error(), Status: http.StatusInternalServerError}
+	}
+
+	return api.WriteJSON(w, http.StatusOK, "")
+}
