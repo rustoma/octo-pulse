e7dd036b98e31821b33b0ed1332408e609b68d1e
rustoma <rutoma00@gmail.com>
2023-09-22 06:56:06 +0200

    feat: add generate description route

--- internal/routes/api_routes.go
+++ internal/routes/api_routes.go
@@ -9,22 +9,24 @@ import (
 	"github.com/rustoma/octo-pulse/internal/controllers"
 	m "github.com/rustoma/octo-pulse/internal/middleware"
 	"github.com/rustoma/octo-pulse/internal/services"
+	"github.com/rustoma/octo-pulse/internal/tasks"
 )
 
 type ApiControllers struct {
-	Auth *controllers.AuthController
+	Auth    *controllers.AuthController
+	Article *controllers.ArticleController
 }
 
 type ApiServices struct {
 	Auth services.AuthService
 }
 
-func NewApiRoutes(controllers ApiControllers, services ApiServices) http.Handler {
+func NewApiRoutes(controllers ApiControllers, services ApiServices, tasks *tasks.Tasks) http.Handler {
 	middlewares := m.NewMiddleware(services.Auth)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
-	//r.Use(app.enableCORS)
+	r.Use(middlewares.EnableCORS)
 
 	r.Route("/api/v1", func(r chi.Router) {
 		//AUTH
@@ -34,9 +36,10 @@ func NewApiRoutes(controllers ApiControllers, services ApiServices) http.Handler
 	})
 	//r.Get("/assets/images/*", api.MakeHTTPHandler(controllers.HandleGetImage))
 
-	r.Route("/api/v1/dashboard", func(mux chi.Router) {
-		mux.Use(middlewares.RequireAuth())
-
+	r.Route("/api/v1/dashboard", func(r chi.Router) {
+		r.Use(middlewares.RequireAuth())
+		r.Use(middlewares.RequireApiKey)
+		r.Get("/article/{id}/generate-description", api.MakeHTTPHandler(controllers.Article.HandleGenerateDescritption))
 	})
 
 	return r
