476711f8bb60f8deeb9b15ad659947ef5fb03fc5
rustoma <rutoma00@gmail.com>
2023-10-14 16:43:13 +0200

    feat: impelment scrapper service

--- internal/services/scrapper.go
+++ internal/services/scrapper.go
@@ -3,22 +3,18 @@ package services
 import (
 	"github.com/rustoma/octo-pulse/internal/models"
 	"github.com/rustoma/octo-pulse/internal/storage"
+	"github.com/rustoma/octo-pulse/internal/validator"
 )
 
 type ScrapperService interface {
 	GetQuestion(id int) (*models.Question, error)
+	GetQuestions(filters ...*storage.GetQuestionsFilters) ([]*models.Question, error)
+	UpdateQuestion(id int, question *models.Question) error
 }
 
 type scrapperService struct {
-	scrapperStore storage.ScrapperStore
-}
-
-type Question struct {
-	Id          int    `json:"id"`
-	Question    string `json:"question"`
-	Answer      string `json:"answer"`
-	Href        string `json:"href"`
-	PageContent string `json:"page_content"`
+	scrapperStore     storage.ScrapperStore
+	scrapperValidator validator.ScrapperValidatorer
 }
 
 type Category int
@@ -28,45 +24,29 @@ const (
 	Gastronomia
 )
 
-type GetQuestionResponse struct {
-	Question Question `json:"question"`
-}
-
-func NewScrapperService(scrapperStore storage.ScrapperStore) ScrapperService {
+func NewScrapperService(scrapperStore storage.ScrapperStore, scrapperValidator validator.ScrapperValidatorer) ScrapperService {
 	return &scrapperService{
-		scrapperStore: scrapperStore,
+		scrapperStore:     scrapperStore,
+		scrapperValidator: scrapperValidator,
 	}
 }
 
 func (s *scrapperService) GetQuestion(id int) (*models.Question, error) {
 	question, err := s.scrapperStore.GetQuestion(id)
-	// req, err := http.NewRequest("GET", fmt.Sprintf("%s/question/%d", s.baseUrl, id), nil)
-	// req.Header.Set("Authorization", os.Getenv("BOT_API_KEY"))
-	// req.Header.Set("Content-Type", "application/json")
-
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// defer resp.Body.Close()
-
-	// body, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// if resp.Status != "200" {
-	// 	var responseError struct{ Message string }
-	// 	err = json.Unmarshal(body, &responseError)
-	// 	return nil, errors.New(responseError.Message)
-	// }
-
-	// var question GetQuestionResponse
-	// err = json.Unmarshal(body, &question)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
 	return question, err
+}
+
+func (s *scrapperService) GetQuestions(filters ...*storage.GetQuestionsFilters) ([]*models.Question, error) {
+	questions, err := s.scrapperStore.GetQuestions(filters...)
+	return questions, err
+}
+
+func (s *scrapperService) UpdateQuestion(id int, question *models.Question) error {
+	err := s.scrapperValidator.Validate(question)
+
+	if err != nil {
+		return err
+	}
+
+	return s.scrapperStore.UpdateQuestion(id, question)
 }
