46f9110e57b9e558717f6dda7e6d659fb61468d4
rustoma <rutoma00@gmail.com>
2023-10-14 16:42:06 +0200

    feat: add GenerateArticlesRequest to task DTO

--- internal/dto/task.go
+++ internal/dto/task.go
@@ -4,3 +4,9 @@ type GetTasksInfoRequest struct {
 	Queue   string   `json:"queue"`
 	TaskIds []string `json:"taskIds"`
 }
+
+type GenerateArticlesRequest struct {
+	DomainId           int `json:"domainId"`
+	NumberOfArtilces   int `json:"numberOfArtilces"`
+	QuestionCategoryId int `json:"questionCategoryId"`
+}
