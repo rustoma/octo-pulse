4fc8d4b401b8520d3cbdc5c965f8aa11f4721983
rustoma <rutoma00@gmail.com>
2023-10-14 16:37:21 +0200

    feat: add scrapper validator

---
+++ internal/validator/scrapper.go
@@ -0,0 +1,41 @@
+package validator
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/rustoma/octo-pulse/internal/errors"
+	"github.com/rustoma/octo-pulse/internal/models"
+)
+
+type scrapperValidator struct {
+	validate *validator.Validate
+}
+
+func newScrapperValidator(validate *validator.Validate) *scrapperValidator {
+	return &scrapperValidator{
+		validate: validate,
+	}
+}
+
+type ScrapperValidation struct {
+	Question    string `validate:"required"`
+	Answear     string `validate:"required"`
+	Href        string `validate:"required"`
+	PageContent string `validate:"required"`
+}
+
+func (v *scrapperValidator) Validate(question *models.Question) error {
+	propertiesToValidate := ScrapperValidation{
+		Question:    question.Question,
+		Answear:     question.Answear,
+		Href:        question.Href,
+		PageContent: question.PageContent,
+	}
+
+	err := v.validate.Struct(propertiesToValidate)
+
+	if err != nil {
+		return errors.BadRequest{Err: err.Error()}
+	}
+
+	return nil
+}
--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -6,17 +6,23 @@ import (
 )
 
 type Validator struct {
-	Article ArticleValidatorer
+	Article  ArticleValidatorer
+	Scrapper ScrapperValidatorer
 }
 
 func NewValidator() *Validator {
 	validate := validator.New(validator.WithRequiredStructEnabled())
 
 	return &Validator{
-		Article: newArticleValidator(validate),
+		Article:  newArticleValidator(validate),
+		Scrapper: newScrapperValidator(validate),
 	}
 }
 
 type ArticleValidatorer interface {
 	Validate(article *models.Article) error
+}
+
+type ScrapperValidatorer interface {
+	Validate(question *models.Question) error
 }
