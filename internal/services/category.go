ca73e173f83cd585b7c84a3c3be64891fc17bd06
rustoma <rutoma00@gmail.com>
2023-10-14 16:42:36 +0200

    feat: add GetDomainCategories to category service

--- internal/services/category.go
+++ internal/services/category.go
@@ -8,14 +8,16 @@ import (
 type CategoryService interface {
 	GetCategories() ([]*models.Category, error)
 	GetCategory(id int) (*models.Category, error)
+	GetDomainCategories(domainId int) ([]*models.Category, error)
 }
 
 type categoryService struct {
-	categoryStore storage.CategoryStore
+	categoryStore          storage.CategoryStore
+	categoriesDomainsStore storage.CategoriesDomainsStore
 }
 
-func NewCategoryService(categoryStore storage.CategoryStore) CategoryService {
-	return &categoryService{categoryStore: categoryStore}
+func NewCategoryService(categoryStore storage.CategoryStore, categoriesDomainsStore storage.CategoriesDomainsStore) CategoryService {
+	return &categoryService{categoryStore: categoryStore, categoriesDomainsStore: categoriesDomainsStore}
 }
 
 func (s *categoryService) GetCategories() ([]*models.Category, error) {
@@ -24,4 +26,22 @@ func (s *categoryService) GetCategories() ([]*models.Category, error) {
 
 func (s *categoryService) GetCategory(id int) (*models.Category, error) {
 	return s.categoryStore.GetCategory(id)
+}
+
+func (s *categoryService) GetDomainCategories(domainId int) ([]*models.Category, error) {
+	categoryIds, err := s.categoriesDomainsStore.GetDomainCategories(domainId)
+	if err != nil {
+		return nil, err
+	}
+
+	var categories []*models.Category
+	for _, categoryId := range categoryIds {
+		category, err := s.categoryStore.GetCategory(categoryId)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
 }
