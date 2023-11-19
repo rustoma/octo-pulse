517d28f71077e017f3586cfb9a92bce3fb79dda1
rustoma <rutoma00@gmail.com>
2023-11-19 08:13:59 +0100

    feat: add QuestionSource model

--- internal/models/question.go
+++ internal/models/question.go
@@ -1,11 +1,19 @@
 package models
 
 type Question struct {
+	Id          int               `json:"id"`
+	Question    string            `json:"question"`
+	Answer      string            `json:"answer"`
+	Href        string            `json:"href"`
+	PageContent string            `json:"pageContent"`
+	Fetched     int               `json:"fetched"`
+	CategoryId  int               `json:"categoryId"`
+	Sources     []*QuestionSource `json:"sources"`
+}
+
+type QuestionSource struct {
 	Id          int    `json:"id"`
-	Question    string `json:"question"`
-	Answear     string `json:"answear"`
+	QuestionId  int    `json:"questionId"`
 	Href        string `json:"href"`
 	PageContent string `json:"pageContent"`
-	Fetched     int    `json:"fetched"`
-	CategoryId  int    `json:"categoryId"`
 }
