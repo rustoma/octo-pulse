0bdb2d2d93e930e4fef9cfc448719b8440e6954e
rustoma <rutoma00@gmail.com>
2023-12-12 18:46:45 +0100

    feat: add image and image category models

---
+++ internal/models/image.go
@@ -0,0 +1,17 @@
+package models
+
+import "time"
+
+type Image struct {
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	Path       string    `json:"path"`
+	Size       int       `json:"size"`
+	Type       bool      `json:"type"`
+	Width      int       `json:"width"`
+	Height     int       `json:"height"`
+	Alt        int       `json:"alt"`
+	CategoryId int       `json:"categoryId"`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
+}
