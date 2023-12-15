344ff488c19c840f76d7f4f0d98a513f41e7869a
rustoma <rutoma00@gmail.com>
2023-12-15 06:12:42 +0100

    fix article dto

--- internal/dto/article.go
+++ internal/dto/article.go
@@ -1,29 +1,35 @@
 package dto
 
-import "time"
+import (
+	"github.com/rustoma/octo-pulse/internal/models"
+	"time"
+)
 
 type Article struct {
-	ID              int       `json:"id"`
-	Title           string    `json:"title" validate:"required,min=4"`
-	Description     string    `json:"description" validate:"required,min=4"`
-	ImageUrl        string    `json:"imageUrl" validate:"required,min=4"`
-	PublicationDate time.Time `json:"publicationDate" validate:"required,min=4"`
-	IsPublished     bool      `json:"isPublished" validate:"required,min=4"`
-	AuthorId        int       `json:"authorId" validate:"required,min=4"`
-	CategoryId      int       `json:"categoryId" validate:"required,min=4"`
-	DomainId        int       `json:"domainId" validate:"required,min=4"`
-	CreatedAt       time.Time `json:"createdAt" validate:"required,min=4"`
-	UpdatedAt       time.Time `json:"UpdatedAt" validate:"required,min=4"`
+	ID              int             `json:"id"`
+	Title           string          `json:"title" validate:"required,min=4"`
+	Slug            string          `json:"slug" validate:"required"`
+	Description     string          `json:"description" validate:"required,min=4"`
+	Thumbnail       *models.Image   `json:"thumbnail" validate:"required"`
+	PublicationDate time.Time       `json:"publicationDate" validate:"required,min=4"`
+	IsPublished     bool            `json:"isPublished" validate:"required,min=4"`
+	Author          models.Author   `json:"author" validate:"required"`
+	Category        models.Category `json:"category" validate:"required"`
+	DomainId        int             `json:"domainId" validate:"required,min=4"`
+	Featured        bool            `json:"featured"`
+	CreatedAt       time.Time       `json:"createdAt" validate:"required,min=4"`
+	UpdatedAt       time.Time       `json:"UpdatedAt" validate:"required,min=4"`
 }
 
 type ArticleValidationErrors struct {
 	Title           string `json:"title"`
+	Slug            string `json:"slug"`
 	Description     string `json:"description"`
-	ImageUrl        string `json:"imageUrl"`
+	Thumbnail       string `json:"thumbnail"`
 	PublicationDate string `json:"publicationDate"`
 	IsPublished     string `json:"isPublished"`
-	AuthorId        string `json:"authorId"`
-	CategoryId      string `json:"categoryId"`
+	Author          string `json:"author"`
+	Category        string `json:"category"`
 	DomainId        string `json:"domainId"`
 	CreatedAt       string `json:"createdAt"`
 	UpdatedAt       string `json:"UpdatedAt"`
