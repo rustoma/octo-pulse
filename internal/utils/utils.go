9b6d28ad6188524d846a950be22fe43480b9054a
rustoma <rutoma00@gmail.com>
2023-12-22 20:56:32 +0100

    feat: add RenameFilesUsingSlug and CalculateReadTime util functions

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,6 +1,13 @@
 package utils
 
-import "os"
+import (
+	"github.com/gosimple/slug"
+	"math"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
 
 func IsProdDev() bool {
 	if os.Getenv("APP_ENV") == "prod" {
@@ -21,4 +28,44 @@ func Every[T comparable](slice []T, callback func(value T, index int) bool) bool
 	}
 
 	return true
+}
+
+func CalculateReadTime(text string) int {
+	words := strings.Fields(text)
+	wordCount := len(words)
+
+	readTime := float64(wordCount) / 200.0                    // Calculate read time in minutes
+	minutes := int(readTime)                                  // Extract whole minutes
+	seconds := math.Round((readTime - float64(minutes)) * 60) // Calculate seconds
+
+	// Round the time
+	if seconds >= 30 {
+		minutes++
+	}
+
+	return minutes
+}
+
+func RenameFilesUsingSlug(dirPath string) {
+
+	list, err := os.ReadDir(dirPath)
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range list {
+
+		if file.Name() == ".DS_Store" {
+			continue
+		}
+
+		name := file.Name()
+
+		filename := path.Base(name)
+		extension := path.Ext(name)
+		filenameWithoutExt := filename[:len(filename)-len(extension)]
+
+		newName := slug.Make(filenameWithoutExt) + extension
+
+		os.Rename(filepath.Join(dirPath, name), filepath.Join(dirPath, newName))
+	}
 }
